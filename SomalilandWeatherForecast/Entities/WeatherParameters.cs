b998b264dba85be203d3400c01324118a7356d07
ayaanlehashi11 <ayaanlemohamed540@gmail.com>
2023-12-16 07:51:43 -0800

    commit

---
+++ SomalilandWeatherForecast/Entities/WeatherParameters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomalilandWeatherForecast.Entities
+{
+    internal class WeatherParameters
+    {
+        public WeatherParameters()
+        { 
+
+        }
+        
+        public string 
+        {
+            set;get;
+        }
+    }
+}
