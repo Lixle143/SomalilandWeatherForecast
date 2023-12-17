f52719896ec7b0329d2c526c0267c5f6a3096c25
ayaanlehashi11 <ayaanlemohamed540@gmail.com>
2023-12-17 01:51:47 -0800

    moving IDataScrapper to Interface folder

--- SomalilandWeatherForecast/Interfaces/IDataScrapper.cs
+++ SomalilandWeatherForecast/Interfaces/IDataScrapper.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SomalilandWeatherForecast.Api
+namespace SomalilandWeatherForecast.Interfaces
 {
     internal interface IDataScrapper
     {
         // private string header = "";
 
-        public string ConvertData2CSV(Dictionary<string  ,string>data);
+        public string ConvertData2CSV(Dictionary<string, string> data);
         public void ParseHTML(string html_data);
         //string Header { get; set; }
     }
